a95a00444f2e024c2860deb21e24a3f3eb5892ef
donald.j.patterson.iii <donald.j.patterson.iii@c4165c6a-0b68-11df-b905-2f917c5cb467>
2010-02-02 17:42:59 +0000

    Updating documentation for IteratorWorker

--- trunk/src/edu/uci/ics/luci/lucicabinet/IteratorWorker.java
+++ trunk/src/edu/uci/ics/luci/lucicabinet/IteratorWorker.java
@@ -2,25 +2,23 @@ package edu.uci.ics.luci.lucicabinet;
 
 import java.io.Serializable;
 
-
 /** This is a class that is used by luci-cabinet to iterate over synchronized sets.
- * It is used by passing it to a database.  The database iterates overs it's members and
- * executes the IteratorWorkers methods in turn. It should be subclassed and the appropriate methods
+ * One uses it by passing it to a database.  The database iterates overs it's members and
+ * executes the IteratorWorker's methods in turn. It should be subclassed and the appropriate methods
  * overridden.
  * 
- * The initialize method is called once before iteration.  It is passed a reference to the database that
+ * The <pre>initialize</pre> method is called once before iteration.  It is passed a reference to the database that
  * is being iterated over.  It may write to the database. 
  * 
- * The iterate function is called once for every element in the database.  It may not write to the database. 
+ * The <pre>iterate</pre> function is called once for every element in the database.  It may not write to the database. 
  * If it tries to, then it will lock indefinitely.
  * 
- * The shutdown method is called once at the end of the iteration.  It is passed a reference to the database 
+ * The <pre>shutdown</pre> method is called once at the end of the iteration.  It is passed a reference to the database 
  * that was just iterated over.  It may write to the database.
  * 
- *  Although there will be no changes made to the database between calls to iterate.  There may be changes to
- * the underlying database after initialize is called and before the first call to iterate.  Likewise, there
- * may be changes to the database after the last call to iterate and the call to shutdown.
- * @author djp3
+ *  Although there will be no changes made to the database between while the <pre>initialize</pre> through the end of the
+ *  <pre>iterate</pre> method, there may be changes to
+ * the underlying database after the completion of <pre>iterate</pre> and the call to shutdown.
  *
  */
 public class IteratorWorker implements Serializable{
