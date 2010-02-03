8769d51fa23f3f6823c9f0b71d88fc043d5f5afb
donald.j.patterson.iii <donald.j.patterson.iii@c4165c6a-0b68-11df-b905-2f917c5cb467>
2010-02-03 20:13:05 +0000

    Adding support for BDB backing database

---
+++ src/edu/uci/ics/luci/lucicabinet/BDB_LUCI_Remote.java
@@ -0,0 +1,35 @@
+package edu.uci.ics.luci.lucicabinet;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This is a convenience class for specifying that the remote database is a tokyo cabinet B-Tree Database. This 
+ * interface can't enforce that the remote implementation is actually BDB, however. 
+ *
+ */
+public class BDB_LUCI_Remote extends DB_LUCI_Remote{
+	
+	private static transient volatile Logger log = null;
+	public Logger getLog(){
+		if(log == null){
+			log = Logger.getLogger(BDB_LUCI_Remote.class);
+		}
+		return log;
+	}
+	
+	/**
+	 *  This method opens a socket connection and read/write stream to a remote server running a Butler service.
+	 * @param host The remote host to connect to, e.g. "localhost", "192.128.1.20"
+	 * @param port The port that the remote host is listening on.
+	 * @throws UnknownHostException
+	 * @throws IOException thrown if the remote host doesn't respond with the expected handshake.
+	 */
+	public BDB_LUCI_Remote(String host,Integer port) throws UnknownHostException, IOException {
+		super(host,port);
+		open(host);
+	}
+
+}
